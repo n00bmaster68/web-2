f2ea937481e409f74d41227373e704657be49c0c
gia_thuan <giathuan2388@gmail.com>
2021-04-29 17:02:40 +0700

    update message confirm

--- html/adminPage.php
+++ html/adminPage.php
@@ -252,7 +252,7 @@
                         </button>
                     </div>
                     <div class="modal-body" id="message-confirm">
-                        
+                    Are you sure you want to delete ?
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal"
