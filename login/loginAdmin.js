b8fe13e45ae962ffa483cfa02ffbe4f07c115c0c
n00bmaster68 <63935745+n00bmaster68@users.noreply.github.com>
2021-05-04 10:23:24 +0700

    check

---
+++ login/loginAdmin.js
@@ -0,0 +1,105 @@
+var response={};
+function renderInfo(){
+    console.log(1);
+}
+
+function loadPage(name)
+{
+    document.getElementById('addProductForm').style.top = "-300%";
+    showBill();
+    document.getElementById("admin").innerHTML = name + "<span>Co-founder and owner</span>"
+}
+
+function checkCookie(){
+    var xml=new XMLHttpRequest();
+    xml.onreadystatechange=function(){
+        if(xml.readyState==4 && xml.status==200){
+            console.log("check cookie");
+            response=JSON.parse(xml.responseText);
+            console.log(response);
+            if(response['status']==1){
+                var flag = 0;
+                if (window.location.href === "http://localhost:8080/project/web-2/html/logInAdmin.html")
+                {
+                    window.location.replace("http://localhost:8080/project/web-2/html/AdminPage.php");
+                    // window.location.href.replace(window.location.search,'');
+                }
+                else
+                {
+                    let n = response['thongtin']['name'].replace(/[\[\]?.,\/#!$%\^&\*;:{}=\\|_~()]/g, "").split(" ");
+                    loadPage(n[n.length - 1]);
+                }
+            }
+
+            else{
+                if(response['status']==-1)
+                {
+                    console.log("hahahaha");
+                     if (window.location.href !== "http://localhost:8080/project/web-2/html/logInAdmin.html")
+                        window.location.replace("http://localhost:8080/project/web-2/html/logInAdmin.html");
+                }
+            }
+        }
+    }
+    xml.open('post','../login/loginAdmin.php');
+    xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xml.send("requestType=1");
+}
+
+window.onload=checkCookie;
+function loginPHP(){
+    if (validate())
+    {
+        var xml=new XMLHttpRequest();
+        let email=document.querySelector('#user_name'),password=document.querySelector('#password');
+        xml.onreadystatechange=function(){
+            if(xml.readyState==4 && xml.status==200){
+                console.log("Login");
+                response=JSON.parse(xml.responseText);
+                console.log(response);
+                // console.log(xml.responseText);
+                if(response['status']==1){
+                    window.location.replace("http://localhost:8080/project/web-2/html/AdminPage.php");
+                }
+                else{
+                    alert("Log in, unsuccessfully");
+                }
+            }
+        }
+        // console.log("truoc: "+email+" "+password);
+        xml.open('post','../login/loginAdmin.php');
+        xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xml.send(`requestType=2&email=${email.value}&pass=${password.value}`);
+    }
+}
+
+function log(){
+    var xml=new XMLHttpRequest();
+    xml.onreadystatechange=function(){
+        if(xml.readyState==4 && xml.status==200){
+            response=JSON.parse(xml.responseText);
+            console.log(xml.responseText);
+
+            if(response['status']==1)
+            {
+                alert("Log out successfully");
+            }
+            else
+                alert("Log out unsuccessfully");
+        }
+    }
+    xml.open('post','../login/loginAdmin.php');
+    xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xml.send("requestType=3");
+}
+
+
+function validate() 
+{
+    var pw = document.getElementById('password').value;
+    var name = document.getElementById('user_name').value;
+
+    if (name === "" || pw === "")
+        return false;
+    return true;
+}
---
+++ login/loginAdmin.php
@@ -0,0 +1,144 @@
+<?php
+session_start();
+$_SESSION['statusLogin'] = 0;
+class loginAdmin
+{
+    public $conn;
+    public $json;
+    public $obj;
+    public $result;
+    public $query;
+
+    function __construct()
+    {
+        $this->connectDB();
+    }
+
+    function connectDB()
+    {
+        $this->conn = new mysqli('localhost', 'root', '', 'ecommerce');
+        if ($this
+            ->conn
+            ->connect_error)
+        {
+            die("connect fail");
+        }
+    }
+
+    function excuteQuery($i = 0)
+    {
+        if ($i == 0)
+        {
+            $this->result = $this
+                ->conn
+                ->query($this->query);
+            if ($this->result->num_rows <1)
+            {
+                $this->result = null;
+            }
+        }
+        else
+        {
+            $this->result = $this
+                ->conn
+                ->query($this->query);
+        }
+    }
+
+    function renderInfo(){
+        $this->json['thongtin']=$_SESSION['thongtin'];
+        $this->json['sp']=$_SESSION['sp'];
+    }
+
+    function checkRequest()
+    {
+        if($_SESSION['statusLogin']==1){
+            $this->json['status']=1;
+            $this->renderInfo();
+            // renderData();
+
+        }
+        else{
+        switch ($_POST['requestType'])
+        {
+            case 1:
+                $this->checkCookie();
+            break;
+            case 2:
+                $this->checkForm();
+            break;
+            default:
+            break;
+        }
+    }
+    }
+
+    function colectInfo()
+    {
+        while ($row = $this
+            ->result
+            ->fetch_assoc())
+        {
+            $this->json['thongtin']['name'] = $row['Ten'];
+            $this->json['thongtin']['maCh'] = $row['MaCh'];
+        }
+    }
+    function checkForm()
+    {
+        $_POST['pass'] = md5($_POST['pass']);
+        $this->query = 'SELECT * FROM nhanvien WHERE Email="'.$_POST['email'].'" AND PassWord="'.$_POST['pass'].'" and tinhtrang=1';
+        // echo $this->query;
+        $this->excuteQuery();
+        $this->json['status'] = 0;
+        if ($this->result != null)
+        {
+            $this->json['status'] = 1;
+            $this->colectInfo();
+            $_SESSION['loginData'] = $this->json;
+            $_SESSION['statusLogin'] = 1;
+            setcookie('login', json_encode($_POST) , time() + 54000, "/");
+        }
+    }
+
+    function logout()
+    {
+        setcookie("login", "", time() - 3600, "/");
+        session_destroy();
+        $this->json['status'] = 1;
+        
+    }
+    function checkCookie()
+    {
+        // echo "run";
+        if (isset($_COOKIE['login']))
+        {
+            $this->json['status'] = 0;
+            $this->obj = json_decode($_COOKIE['login'], true);
+            $this->query = 'SELECT * FROM nhanvien WHERE Email="'.$this->obj['email'].'" AND PassWord="'.$this->obj['pass'].'" and tinhtrang=1';
+            // echo $this->query;
+            $this->excuteQuery();
+            if ($this->result != null)
+            {
+                $this->colectInfo();
+                $this->json['status'] = 1;
+
+                $_SESSION['loginData'] = $this->json;
+                $_SESSION['statusLogin'] = 1;
+            }
+        }
+        else
+        {
+            $this->json['status'] = - 1;
+        }
+    }
+
+    function getJson()
+    {
+        return $this->json;
+    }
+}
+
+$login = new loginAdmin();
+$login->checkRequest();
+echo json_encode($login->getJson());
+?>
