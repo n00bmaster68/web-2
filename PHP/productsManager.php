badafe35e48a1cacbfc2067b70506192e826cca8
gia_thuan <giathuan2388@gmail.com>
2021-05-12 10:01:33 +0700

    update final

--- PHP/productsManager.php
+++ PHP/productsManager.php
@@ -55,30 +55,32 @@
         $type = $_POST["updateType"];
         $image = "";
 
-        //upload file
-        $file = $_FILES['productImage'];
-
-        $fileName = $_FILES['productImage']['name'];
-        $fileTmpName = $_FILES['productImage']['tmp_name'];
-        $fileSize = $_FILES['productImage']['size'];
-        $fileError = $_FILES['productImage']['error'];
-        $fileType = $_FILES['productImage']['type'];
-        
-        $fileExt = explode('.',$fileName);
-        $fileActualExt = strtolower(end($fileExt));
-        $allowed = array('jpg','jpeg','png','pdf');
-
-        if (in_array($fileActualExt,$allowed)) {
-            if ($fileError === 0){
-                $fileNameNew = uniqid('',true).".".$fileActualExt;
-                $fileDestination = '../image/'.$fileNameNew;
-                $image = $image . "../image/".$fileNameNew;
-                move_uploaded_file($fileTmpName, $fileDestination);
+        if (isset($_FILES['productImage'])){
+            //upload file
+            $file = $_FILES['productImage'];
+    
+            $fileName = $_FILES['productImage']['name'];
+            $fileTmpName = $_FILES['productImage']['tmp_name'];
+            $fileSize = $_FILES['productImage']['size'];
+            $fileError = $_FILES['productImage']['error'];
+            $fileType = $_FILES['productImage']['type'];
+            
+            $fileExt = explode('.',$fileName);
+            $fileActualExt = strtolower(end($fileExt));
+            $allowed = array('jpg','jpeg','png','pdf');
+    
+            if (in_array($fileActualExt,$allowed)) {
+                if ($fileError === 0){
+                    $fileNameNew = uniqid('',true).".".$fileActualExt;
+                    $fileDestination = '../image/'.$fileNameNew;
+                    $image = $image . "../image/".$fileNameNew;
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                } else {
+                    echo "There was an error uploading your file!";
+                }
             } else {
-                echo "There was an error uploading your file!";
+                echo "You cannot upload file of this type !";
             }
-        } else {
-            echo "You cannot upload file of this type !";
         }
         
         if(!empty($id)) {
